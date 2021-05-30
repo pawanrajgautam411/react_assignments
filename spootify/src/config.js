ba0ba6be8d2a467fd38e6c257700273b07cf96f2
pawanrajgautam411 <pawan.raj.gautam411@gmail.com>
2021-05-30 19:38:37 +0530

    Initial Commit
    Initial Commit

---
+++ spootify/src/config.js
@@ -0,0 +1,8 @@
+export default {
+  api: {
+    baseUrl: 'https://api.spotify.com/v1',
+    authUrl: 'https://accounts.spotify.com/api/token',
+    clientId: 'a395157811664d9fa9c8536e6e8816d9',
+    clientSecret: '70b41f764b824c9fbb9509f3c8118776'
+  }
+}
